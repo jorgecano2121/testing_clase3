948afe228980a6b57953edec186582fd2dbeddec
jorge <jorgealexiscano@gmail.com>
2021-04-09 14:03:09 -0300

    resolucion de ejercicios para corregir

---
+++ clases.py
@@ -0,0 +1,31 @@
+class superficie_rectangulo :
+    def _init_(self, b,a):
+        self._base = b
+        self._altura = a
+
+    def multiplicacionbaseporaltura():
+        resultado = self.base * self.altura
+        return resultado
+
+print.obj multiplicacionbaseporaltura(3,6)
+
+    
+
+
+
+#class superficie_triangulorectangulo:
+ #   def _init_(self,b,a):
+ #       self._base = b
+  #      self.altura = a
+
+   # def operacionbaseporaltura():
+    #    resultado = self.base * self.altura
+     ##   return resutado 
+
+
+
+#class coordenada 
+
+
+
+
