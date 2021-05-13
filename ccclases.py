591a375cb4ffbe5ff4d9f8c033486e0a04e4b2ff
jorge <jorgealexiscano@gmail.com>
2021-05-13 00:50:00 -0300

    perdon la demora hasta que me salio

---
+++ ccclases.py
@@ -0,0 +1,37 @@
+class superficie_rectangulo :
+    def _init_(self, b,a):
+        self.base = b
+        self.altura = a
+
+    def multiplicacionbaseporaltura():
+        resultado = self.base * self.altura
+        return resultado
+
+
+
+    
+
+
+
+class Superficie_triangulorectangulo:
+    def _init_(self,b,a):
+        self.base = b
+        self.altura = a
+
+    def Operacionbaseporaltura():
+        resultado = self.base * self.altura
+        return resutado 
+
+
+
+class Coordenada:
+    def __init__(self ,x,e):
+        self.punto1 = x
+        self.punto2 = e 
+
+    def calcularcoordenada():
+        resultado = self.punto1 + self.punto2 / 2
+        return resultado 
+
+
+
